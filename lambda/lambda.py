65833f71878c16bf8fe04214c5e5e01593f2861a
mia-thomas <miathomas1102@gmail.com>
2022-01-05 09:43:18 +0000

    ADD - new changes upload.

--- lambda/lambda.py
+++ lambda/lambda.py
@@ -1,10 +1,6 @@
 import logging
 import boto3
+from botocore.exceptions import ClientError
+import os
 
 s3 = boto3.client("s3")
-src_key = "my-key"
-src_bucket = "my-bucket"
-s3.copy_object(Key=src_key, Bucket=src_bucket,
-               CopySource={"Bucket": src_bucket, "Key": src_key},
-               Metadata={"my_new_key": "my_new_val"},
-               MetadataDirective="REPLACE")
